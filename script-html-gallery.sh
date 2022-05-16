78363e4bf345c1a2483d71baf1a0047c0533cb72
Peter <peterthomaskim@gmail.com>
2022-05-16 01:34:24 -0300

    lazy img addded

--- script-html-gallery.sh
+++ script-html-gallery.sh
@@ -3,6 +3,12 @@ i=1
 
 until [ $i -gt 95 ]
 do
-    echo "<li><a href='/assets/gallery/$i.jpg'><img src='assets/gallery/$i.jpg' alt='image da galeria'></a></li>"
+	if [ $i -gt 10 ]
+	then
+    echo "<li><a href='/assets/gallery/$i.jpg'><img loading='lazy' src='assets/gallery/$i.jpg' alt='image da galeria'></a></li>"
     ((i=i+1))
+	else
+	echo "<li><a href='/assets/gallery/$i.jpg'><img src='assets/gallery/$i.jpg' alt='image da galeria'></a></li>"
+    ((i=i+1))
+	fi
 done
