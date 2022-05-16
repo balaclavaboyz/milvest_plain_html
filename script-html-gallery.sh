c74cf0b3c0ef1ad0ac0a11e254b354c7e0ff87f3
Peter <peterthomaskim@gmail.com>
2022-05-16 12:35:28 -0300

    coverted to webp images

--- script-html-gallery.sh
+++ script-html-gallery.sh
@@ -5,10 +5,10 @@ until [ $i -gt 95 ]
 do
 	if [ $i -gt 10 ]
 	then
-    echo "<li><a href='/assets/gallery/$i.jpg'><img loading='lazy' src='assets/gallery/$i.jpg' alt='image da galeria'></a></li>"
+    echo "<li><a href='/assets/webp/$i.webp'><img loading='lazy' src='assets/webp/$i.webp' alt='image da galeria'></a></li>"
     ((i=i+1))
 	else
-	echo "<li><a href='/assets/gallery/$i.jpg'><img src='assets/gallery/$i.jpg' alt='image da galeria'></a></li>"
+	echo "<li><a href='/assets/webp/$i.webp'><img src='assets/webp/$i.webp' alt='image da galeria'></a></li>"
     ((i=i+1))
 	fi
 done
